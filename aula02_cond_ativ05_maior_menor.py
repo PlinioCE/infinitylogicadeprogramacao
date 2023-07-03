fe9722e0ca7508acd7e819d8bf94fee7ca0887e5
Plinio Albuquerque <pliniomalbuquerque@gmail.com>
2023-07-03 17:12:59 -0300

    Melhorando programa adicionando verificacao de numeros iguais.

--- aula02_cond_ativ05_maior_menor.py
+++ aula02_cond_ativ05_maior_menor.py
@@ -6,17 +6,45 @@ numero1 = int(input('Digite o 1º número: '))
 numero2 = int(input('Digite o 2º número: '))
 numero3 = int(input('Digite o 3º número: '))
 
-if numero1 > numero2 and numero1 > numero3:
-    maior = numero1
-elif numero2 > numero1 and numero2 > numero3:
-    maior = numero2
-else:
-    maior = numero3
+if numero1 != numero2 and numero1 != numero3 and numero2 != numero3:
+    if numero1 > numero2 and numero1 > numero3:
+        maior = numero1
+    elif numero2 > numero1 and numero2 > numero3:
+        maior = numero2
+    else:
+        maior = numero3
 
-if numero1 < numero2 and numero1 < numero3:
-    menor = numero1
-elif numero2 < numero1 and numero2 < numero3:
-    menor = numero2
+    if numero1 < numero2 and numero1 < numero3:
+        menor = numero1
+    elif numero2 < numero1 and numero2 < numero3:
+        menor = numero2
+    else:
+        menor = numero3
+    print(f'\nO maior número é {maior} e o menor é {menor}.')
+
+elif numero1 == numero2 and numero1 != numero3 and numero2 != numero3:
+    if numero1 > numero3:
+        maior = numero1
+        menor = numero3
+    else:
+        maior = numero3
+        menor = numero1
+    print(f'\nO maior número é {maior} e o menor é {menor} e o primeiro número é igual ao segundo.')
+elif numero1 != numero2 and numero1 == numero3 and numero2 != numero3:
+    if numero1 > numero2:
+        maior = numero1
+        menor = numero2
+    else:
+        maior = numero2
+        menor = numero1
+    print(f'\nO maior número é {maior} e o menor é {menor} e o primeiro número é igual ao terceiro.')
+elif numero1 != numero2 and numero1 != numero3 and numero2 == numero3:
+    if numero1 > numero2:
+        maior = numero1
+        menor = numero2
+    else:
+        maior = numero2
+        menor = numero1
+    print(f'\nO maior número é {maior} e o menor é {menor} e o segundo número é igual ao terceiro.')
 else:
-    menor = numero3
-print(f'\nO maior número é {maior} e o menor é {menor}.')
+    print(f'\nTodos os números são iguais.')
